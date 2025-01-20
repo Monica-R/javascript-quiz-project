5ac0914f4bf30f673000aafe6c5843f4b887e181
Monica-R <moniroka_339@hotmail.com>
2025-01-20 17:55:22 +0000

    Mini project in progress

--- src/question.js
+++ src/question.js
@@ -1,7 +1,24 @@
 class Question {
     // YOUR CODE HERE:
-    //
+    constructor(text, choices, answer, difficulty){
+        this.text= text;
+        this.choices= choices;
+        this.answer = answer;
+        this.difficulty = difficulty;
+    }
+
     // 1. constructor (text, choices, answer, difficulty)
 
     // 2. shuffleChoices()
+    shuffleChoices(){
+      let arrayChoiceSize = this.choices.length;
+      
+      while(arrayChoiceSize !== 0) {
+        let randomPos = Math.floor(Math.random() * arrayChoiceSize);
+        arrayChoiceSize--;
+        //swapping
+        [this.choices[arrayChoiceSize], this.choices[randomPos]] = [this.choices[randomPos], this.choices[arrayChoiceSize]];
+      }
+
+    }
 }
--- src/quiz.js
+++ src/quiz.js
@@ -1,15 +1,46 @@
 class Quiz {
     // YOUR CODE HERE:
-    //
     // 1. constructor (questions, timeLimit, timeRemaining)
+    constructor(questions, timeLimit, timeRemaining) {
+        this.questions = questions;
+        this.timeLimit = timeLimit;
+        this.timeRemaining = timeRemaining;
+        this.correctAnswers = 0;
+        this.currentQuestionIndex = 0;
+    }
 
     // 2. getQuestion()
+    getQuestion() {
+        return this.questions[this.currentQuestionIndex];
+    }
     
     // 3. moveToNextQuestion()
+    moveToNextQuestion() {
+        this.currentQuestionIndex++;
+    }
 
     // 4. shuffleQuestions()
+    shuffleQuestions() {
+        let arrayQuestionSize = this.questions.length;
+
+        while (arrayQuestionSize !== 0) {
+            let randomPosQuestion = Math.floor(Math.random() * arrayQuestionSize);
+            arrayQuestionSize--;
+
+            [this.questions[arrayQuestionSize], this.questions[randomPosQuestion]] = [this.questions[randomPosQuestion], this.questions[arrayQuestionSize]];
+        }
+    }
 
     // 5. checkAnswer(answer)
+    checkAnswer(answer) {
+        console.log(this.questions);
+        this.questions.forEach((question) => {
+
+            if (this.questions.answer === answer) {
+                this.correctAnswers++;
+            }
+        });
+    }
 
     // 6. hasEnded()
 }
